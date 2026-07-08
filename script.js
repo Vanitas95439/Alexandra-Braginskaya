01efdafb11fc5b7dcbaae24917c7bacdf4f3702d
Vanitas95439 <fantom95439@yandex.ru>
2026-07-08 21:59:59 +0300

    added script for paintings

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-const works = document.querySelectorAll(".work");
+const gallery = document.getElementById("gallery");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    }
+    if (entry.isIntersecting) entry.target.classList.add("show");
   });
 });
 
-works.forEach((work) => {
-  observer.observe(work);
-});
+async function loadGallery() {
+  const list = await fetch("gallery.json");
+  const ids = await list.json();
+
+  for (const id of ids) {
+    const response = await fetch(`data/${id}.json`);
+    const work = await response.json();
+
+    const card = document.createElement("div");
+    card.className = "work";
+
+    card.innerHTML = `
+    <a href="painting.html?id=${id}">
+        <img src="img/${id}.jpg" alt="${work.title}">
+    </a>
+
+    <h3>${work.title}</h3>
+`;
+
+    gallery.appendChild(card);
+
+    observer.observe(card);
+  }
+}
+
+loadGallery();
